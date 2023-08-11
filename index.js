e8d60896f517363f03cde62e199f16a03c6c1851
Yiping Lin <li1234355@gmail.com>
2023-08-11 09:36:45 +0200

    Update code style

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
 const display = document.getElementById('display'),
       buttons = document.querySelectorAll('button');
-var displayValue = 0,
+let displayValue = 0,
     num1 = null,
     num2 = null,
     operator1 = null,
     operator2 = null;
 
-const add = (num1, num2) =>  num1 + num2;
+const add = (num1, num2) => num1 + num2;
 
 const substract = (num1, num2) => num1 - num2;
 
 const multiply = (num1, num2) => num1 * num2;
 
-const divide = (num1, num2) =>{
-    if(num2 == 0){
-        return 'NOT ALLOWED!'
-    }else{
+const divide = (num1, num2) => {
+    if (num2 === 0) {
+        return 'NOT ALLOWED!';
+    } else {
         return num1 / num2;
     }
 };
 
 const operate = (func, num1, num2) => func(num1, num2);
 
-const updateDisplay = () =>{
+const updateDisplay = () => {
     display.innerText = displayValue;
-}
+};
 
-const initialize = () =>{
-    buttons.forEach((button) =>{
-        if(button.classList.contains('operand')){
+const initialize = () => {
+    buttons.forEach((button) => {
+        if (button.classList.contains('operand')) {
             button.addEventListener('click', () => {
                 operand(button.innerText);
                 updateDisplay();
             });
-        }else if(button.classList.contains('operator')){
+        } else if(button.classList.contains('operator')) {
             button.addEventListener('click', () => {
                 console.log("Clicked operator");
             });
-        }else if(button.id == 'btn-ac'){
+        } else if (button.id == 'btn-ac') {
             button.addEventListener('click', () => {
                 console.log("Clicked AC");
             });
-        }else if(button.id == 'backspace'){
+        } else if (button.id == 'backspace') {
             button.addEventListener('click', () => {
-                if(displayValue.toString().length == 1){
+                if (displayValue.toString().length == 1) {
                     displayValue = 0;
-                }else{
+                } else {
                     displayValue = backspace(displayValue);
                 }
                 updateDisplay();
             });
-        }else if(button.classList.contains('decimal')){
+         } else if (button.classList.contains('decimal')) {
             button.addEventListener('click', () => {
                 addFloatingPoint();
                 updateDisplay();
             });
-        }else if(button.id == 'btn-equal'){
+        } else if (button.id == 'btn-equal') {
             button.addEventListener('click', () => {
                 console.log("Clicked =");
             });
@@ -64,16 +64,16 @@ const initialize = () =>{
 }
 
 const operand = (num) =>{
-    if(num1 == null){
-        if(displayValue === 0){
+    if (num1 == null) {
+        if (displayValue === 0) {
             displayValue = num;
-        }else{
+        } else {
             displayValue += num;
         }
-    }else{
-        if(displayValue == firstOperand || displayValue == 0) {
+    } else {
+        if (displayValue === firstOperand || displayValue === 0) {
             displayValue = num;
-        }else{
+        } else{
             displayValue += num;
         }
     }
@@ -82,10 +82,11 @@ const operand = (num) =>{
 const backspace = (numInString) => numInString.slice(0, -1);
 
 const addFloatingPoint = () => {
-    if(displayValue === 0){
+    if (displayValue === 0) {
         displayValue += '.';
-    }else if(!displayValue.includes('.')) {
+    } else if (!displayValue.includes('.')) {
         displayValue += '.';
     }
 }
+
 initialize();
